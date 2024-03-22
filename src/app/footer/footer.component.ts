71fd22f565acf6c6dc09e77922f79d1775449a70
Hanuman Yadav <hanuman2241@gmail.com>
2024-03-22 13:26:07 +0530

    'Marks-Calculation'

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
+  inputs:['name'],
+})
+export class FooterComponent {
+  name:any;
+
+}
